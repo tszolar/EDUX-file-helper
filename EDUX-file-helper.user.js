1d9ef103ce9828a271a80f18e15522b80e4860dc
Tibor Szolár <tibor.szolar@seznam.cz>
2014-05-06 04:03:55 +0200

    Partial implementation

--- EDUX-file-helper.user.js
+++ EDUX-file-helper.user.js
@@ -1,41 +1,67 @@
 // ==UserScript==
-// @name       Edux File Helper
+// @name       EDUX file helper
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.2
 // @description  enter something useful
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://stuk.github.io/jszip/dist/jszip.js
+// @require     http://stuk.github.io/jszip-utils/dist/jszip-utils.js
 // @match      https://edux.fit.cvut.cz/courses/*
 // @copyright  2014+, flex
 // ==/UserScript==
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+
 $(function(){
-	var fetch_namespace_tree = function(course_code, tree_fetch_success) {
-		var namespace = '';
+	//var namespaces_to_search = ["", "lectures", "labs"];
+
+
+	var get_child_namespaces = function(doc, parent_namespace, current_depth) {
+		var child_depth = current_depth + 1;
+		var ns_nodes = doc.querySelectorAll(".media.level" + child_depth + ".closed");
+		return [].map.call(ns_nodes, function(node) {
+			var link = node.querySelector("a.idx_dir");
+			var prefix = parent_namespace;
+			if(prefix != "")
+				prefix += "/";
+			return prefix + link.text;
+		});
+	};
+
+	var process_files = function(file_nodes, namespace) {
+		console.log("Files for "+namespace+":");
+		console.log(file_nodes);
+		//file_nodes.querySelector("");
+
+		[].forEach.call(file_nodes, function(file_node) {
+			var link = file_node.querySelector("a:nth-child(3)");
+			console.log(link.href);
+		});
+	};
+
+	var generate_doc = function(data) {
+		var doc = document.implementation.createHTMLDocument();
+		//Replace scripts
+		data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
+		doc.documentElement.innerHTML = data;
+		return doc;
+	}
+
+	var fetch_ns_info = function(course_code, namespace, depth) {
+		
 		var success = function( data ) {
-			//$(".result").html( data );
-			// alert( "Load was performed." );
-			//console.log(data);
 			
-			var doc = document.implementation.createHTMLDocument();
-			// doc.open();
-			//Replace scripts
-			data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
-			//Write HTML to the new document
-			// doc.write(data);
-			doc.documentElement.innerHTML = data;
-			// doc.close();
-			
-			// console.log(doc.body);  //Empty
+			var doc = generate_doc(data);
 			var file_nodes = doc.querySelectorAll('#media__content div.odd, #media__content div.even');
-			$(file_nodes).find("")
+			process_files(file_nodes, namespace);
 
-			// var file_nodes = doc.$('#media__content div.odd, #media__content div.even');
-			console.log(file_nodes);
+			var child_namespaces = get_child_namespaces(doc, namespace, depth);
+			child_namespaces.forEach(function(ns) {
+				//console.log(ns);
+				fetch_ns_info(course_code, ns, depth + 1);
+			});
 
-			var tree = "";
-			tree_fetch_success(tree);
 		};
 
 		var url = 'https://edux.fit.cvut.cz/courses/'+ course_code + '/lib/exe/mediamanager.php?ns='+namespace;
@@ -45,10 +71,16 @@ $(function(){
 	var course_code = "MI-PIS";
 	var namespace = "lectures";
 	
-	fetch_namespace_tree(course_code, function(tree){
-		console.log(tree)
-	});
+	fetch_ns_info("MI-PIS", "", 0);
 
-	
+	// $.get("https://edux.fit.cvut.cz/courses/MI-SPI/_media/lectures/mi-spi-lec01-probabilityreview.pdf", function(data) {
+
+	// });
+
+	// var zip = new JSZip();
+	// zip.file("Hello.txt", "Hello World\n");
+	// var content = zip.generate({type:"blob"});
+	// // see FileSaver.js
+	// saveAs(content, "example.zip");
 });
 
